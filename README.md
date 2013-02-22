ff23d3109a1fccba836040eda8943ed8ccc4d715
jks <chinatianma#gmail.com>
2013-02-22 22:39:18 +0800

    Add files

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 =====
 
 lcd19264, including ch16264b.
+
+config.h is the only file you should modify.
+lcd19264.h is the only file you should include.
+
---
+++ lcd19264.h
@@ -0,0 +1,30 @@
+#ifndef LCD19264_H
+#define LCD19264_H
+
+#include "config.h"
+
+/* Choose one and comment the others */
+#define CH19264B
+
+int lcd19264_display_string(int row, int column, char *s);
+
+#endif  /* LCD19264_H */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
